26e1e5c8a89eb05119d2e1e44eac49269724dc71
Raidanor <silverstallion30@gmail.com>
2026-04-28 04:16:43 -0300

    homepage components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import {HomeHeroSection} from "../components/HomeHeroSection"
+import { GalleryShowcaseSection } from "@/components/GalleryShowcaseSection";
+import { HowItWorksSection } from "@/components/HowItWorksSection";
+import { Footer } from "@/components/Footer";
+import Testimonials from "@/components/Testimonials";
+import { PricingSection } from "@/components/PricingSection";
 
 export default function Home() {
     return(
         <main className="min-h-screen bg-background p-3 sm:p-4 lg:p-5">
             <HomeHeroSection />
+            <GalleryShowcaseSection />
+            <HowItWorksSection />
+            <PricingSection />
+            <Testimonials />
+            <Footer />
         </main>
     
     );
